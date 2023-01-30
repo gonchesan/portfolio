62e415aa7e0aa6dd4c0c231d4fc67d68a3e12510
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2023-01-30 20:42:18 -0300

    Add project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,13 +3,16 @@ import "../styles/components/projectCard.scss";
 import CalendarIcon from "../assets/icons/Calendar.jsx";
 import LinkExternalIcon from "../assets/icons/LinkExternal";
 import WebsiteIcon from "../assets/icons/Website";
+import useVisibility from "../hooks/useVisibility";
 
 const ProjectCard = ({ projectData }) => {
   const { title, description, pages, date, status, repo, website, tags, img } =
     projectData;
   const [showMore, setShowMore] = useToggle();
+  const [isOpen, cardRef] = useVisibility(50);
+
   return (
-    <div className="card">
+    <div ref={cardRef} className={`card${isOpen ? "--active" : ""}`}>
       <div className="card__aside">
         <span className="card__status">
           <i></i>
@@ -33,16 +36,18 @@ const ProjectCard = ({ projectData }) => {
         <button className="button--toggle" onClick={setShowMore}>
           {showMore ? "Show less" : "Show more"}
         </button>
-        {tags.map((tag) => (
-          <div className="badge" key={tag.icon}>
-            <img
-              className="badge__icon"
-              src={`../src/assets/icons/${tag.icon}.png`}
-              alt={`${tag.text} icon`}
-            />
-            <span className="badge__text">{tag.text}</span>
-          </div>
-        ))}
+        <div className="badge__wrapper">
+          {tags.map((tag) => (
+            <div className="badge" key={tag.icon}>
+              <img
+                className="badge__icon"
+                src={`../src/assets/icons/${tag.icon}.png`}
+                alt={`${tag.text} icon`}
+              />
+              <span className="badge__text">{tag.text}</span>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="card__footer">
         <a href={repo} target="_blank" className="card__button--secondary">
